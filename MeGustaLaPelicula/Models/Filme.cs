ac1941c81dd2302203c673815975323a2b84ad53
Sandra <Sandra@DESKTOP-OEC3L74>
2016-09-21 11:27:18 +0100

    Model: Adicionada a classe Filme que modela os objectos filme utilizados neste projecto

---
+++ MeGustaLaPelicula/Models/Filme.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace MeGustaLaPelicula.Models
+{
+    public class Filme
+    {
+
+        [Required]
+        public int FilmeID { get; set; }
+        [Required]
+        public string Titulo { get; set; }
+        public int Ano { get; set; }
+        [Required]
+        public string Realizador { get; set; }
+        public string Genero { get; set; }
+        public int Classificacao { get; set; }
+    }
+
+    public class FilmeDBContext : DbContext
+    {
+        public DbSet<Filme> Filmes { get; set; }
+    }
+}
