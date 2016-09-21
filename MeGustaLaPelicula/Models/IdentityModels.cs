a4a9ad49751937670d3a64b9ce95833bd6ad97d3
syntecpt <sergiomfcp@hotmail.com>
2016-09-21 12:29:39 +0100

    adicionada referencia Filmes ao dbcontext

--- MeGustaLaPelicula/Models/IdentityModels.cs
+++ MeGustaLaPelicula/Models/IdentityModels.cs
@@ -31,5 +31,7 @@ namespace MeGustaLaPelicula.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<MeGustaLaPelicula.Models.Filme> Filmes { get; set; }
     }
 }
